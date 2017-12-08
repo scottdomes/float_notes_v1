b161f58ccc8dd015dcc9cd889512d93bcbf53d2b
YScott Domes <scott@musefind.com>
2017-12-08 15:15:59 -0800

    Tried to get transitions working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { CSSTransition } from 'react-transition-group';
 
 import logo from './logo.svg';
 import './App.css';
 import { NoteView, NoteListView, LoginView, FolderView } from './containers';
 import { fetchNotes } from './actions';
 
+console.log(CSSTransition)
 class App extends Component {
   componentDidMount() {
     this.props.fetchNotes();
@@ -14,20 +16,29 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <a href="/">Float Notes</a>
-        <Route exact path="/" component={NoteListView} />
-        <Route
-          path="/folders/:name/notes/:id"
-          render={props => <NoteView {...props} />}
-        />
-        <Route
-          exact
-          path="/folders/:name"
-          render={props => <FolderView {...props} />}
-        />
-        <Route path="/login" component={LoginView} />
-      </div>
+      <Route
+        render={({ location }) => (
+          <div className="App">
+            <a href="/">Float Notes</a>
+            <Route exact path="/" component={NoteListView} />
+            <CSSTransition
+              className="fade"
+              timeout={1000}>
+              <Route
+              location={location}
+                path="/folders/:name/notes/:id"
+                render={props => <NoteView {...props} />}
+              />
+            </CSSTransition>
+            <Route
+              exact
+              path="/folders/:name"
+              render={props => <FolderView {...props} />}
+            />
+            <Route path="/login" component={LoginView} />
+          </div>
+        )}
+      />
     );
   }
 }
